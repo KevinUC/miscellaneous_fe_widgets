9a83945f32705d02733d0ba645b3080eb0c6f8a2
kedai <kedai0128@outlook.com>
2020-08-23 22:25:35 -0700

    next-step: support scss, use htmlWebpackPlugin and loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
+    watchContentBase: true,
     //hot: true,
     // overlay: true
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader", options: { injectType: "linkTag" } },
+          //"style-loader",
+          { loader: "file-loader" },
+          //"css-loader",
+        ],
+      },
+
+      // {
+      //   //IMAGE LOADER
+      //   test: /\.png$/,
+      //   loader: "file-loader",
+      // },
+
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
 };
